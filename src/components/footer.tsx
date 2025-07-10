5fcd4e21b86975a2b09b5741237e36ee219f6b97
pawansaisrinivas <153428743+pawansaisrinivas@users.noreply.github.com>
2025-07-10 16:41:16 +0530

    Create footer.tsx

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Footer() {
+  return (
+    <footer className="py-6 w-full text-center text-sm text-muted-foreground">
+      <p>&copy; {new Date().getFullYear()} Pawan Sai Srinivas Panigrahi. All Rights Reserved.</p>
+    </footer>
+  );
+}
